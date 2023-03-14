979834086ced3bcbd7d5a05d269bb81a1e92dc76
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-14 05:58:26 -0300

    first file .java

---
+++ MyFirstClass.java
@@ -0,0 +1,18 @@
+public class MyFirstClass {
+  public static void main(String...strings) {
+    System.out.println(GenerateMesssage.generateMessage());
+    System.out.println(AnotherMessage.anotherMessage());
+  }
+}
+
+class GenerateMesssage {
+  static String generateMessage() {
+    return "I'm learning Java";
+  }
+}
+
+class AnotherMessage {
+  static String anotherMessage() {
+    return "LOL";
+  }
+}
