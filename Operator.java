266f2fed5c3351d755ea53c7ce937c7358da7604
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-16 23:14:33 -0300

    update

---
+++ Operator.java
@@ -0,0 +1,19 @@
+public class Operator {
+  public static void main(String[] args) {
+    int result = 10 + 10;
+    int rest = result % 8;
+    int age1 = 18;
+    int age2 = 17;
+    
+    System.out.println("10 + 10 = " + result);
+
+    System.out.println(rest);
+
+    if(age1 > age2) {
+      System.out.println(age1 + " e maior que " + age2);
+    }
+
+
+
+  }
+}
