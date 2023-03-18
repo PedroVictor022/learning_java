2ec7de7b5c861b56ea08116f6c5b82664311babd
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-18 18:42:01 -0300

    up

---
+++ ArrayCopy.java
@@ -0,0 +1,15 @@
+class ArrayCopy {
+  public static void main(String[] args) {
+   String[] names = {
+    "Pedro", "Victor", "Samuel", "Leo", "Rodrigo", "Maycon", "Luiza"
+   };
+
+   String[] copyNames = new String[6];
+
+   System.arraycopy(names, 2, copyNames, 0, 3);
+  
+    for (String namesL : copyNames) {
+      System.out.print(namesL + " ");
+    }
+  }
+}
